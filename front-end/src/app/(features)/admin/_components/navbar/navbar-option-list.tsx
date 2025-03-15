aeb4e5cdc08e0e2bfd43b4c8ced11101c78cb05a
John Adrian Cruz <jhndrncrz@gmail.com>
2025-03-15 14:16:35 +0800

    feat(WIP): ongoing admin folder structure

---
+++ front-end/src/app/(features)/admin/_components/navbar/navbar-option-list.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { Stack } from "@mantine/core";
+import Link from "next/link";
+import NavbarOption from "./navbar-option";
+import { IconHome, IconSourceCode, IconSchool } from "@tabler/icons-react";
+
+const linksList = [
+  { link: "/dashboard", label: "Dashboard", icon: IconHome },
+  { link: "/courses", label: "Courses", icon: IconSchool},
+  { link: "/playground", label: "Playground", icon: IconSourceCode },
+];
+
+export default function NavbarLinks() {
+  const pathname = usePathname();
+  const [, setActive] = useState(pathname);
+
+  return (
+    <Stack gap="0">
+      {linksList.map((link) => (
+        <Link
+          style={{ textDecoration: "none", color: "darkslategray" }}
+          href={link.link}
+          key={link.label}
+          onClick={() => setActive(link.link)}
+        >
+          <NavbarOption
+            label={link.label}
+            icon={link.icon}
+            isActive={pathname.startsWith(link.link)}
+          />
+        </Link>
+      ))}
+    </Stack>
+  );
+}
