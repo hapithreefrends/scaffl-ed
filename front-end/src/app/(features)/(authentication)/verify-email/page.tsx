262b7c060b584e4dd81ab59924c099e48e09eae4
John Adrian Cruz <jhndrncrz@gmail.com>
2025-04-05 14:05:27 +0800

    feat: update svgs

--- front-end/src/app/(features)/(authentication)/verify-email/page.tsx
+++ front-end/src/app/(features)/(authentication)/verify-email/page.tsx
@@ -16,7 +16,7 @@ import ScaffledLogo from "@/app/_components/ScaffledLogo";
 
 import { createClient } from "@/utilities/supabase/client";
 import { notifications } from "@mantine/notifications";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams, notFound } from "next/navigation";
 
 const supabase = createClient();
 
@@ -32,7 +32,7 @@ export default function Page() {
             const queryEmail = searchParams.get("email");
             console.log("User data:", queryEmail);
             if (!queryEmail) {
-                console.error("No email provided in query parameters");
+                console.error("No email provided in query parameters!");
                 return;
             }
             setEmail(queryEmail);
@@ -75,76 +75,79 @@ export default function Page() {
         return data.user;
     };
 
-
-    return (
-        <Flex
-            justify="center"
-            align="center"
-            style={{ height: "100vh", background: "linear-gradient(to right, #0D6E6E, #683598)" }}
-        >
-            <Paper
-                shadow="md"
-                radius="md"
-                p={{ base: "md", sm: "xl" }}
-                style={{
-                    width: "60%",
-                    background: "white",
-                }}
+    if (!searchParams.get("email")) {
+        return notFound();
+    } else {
+        return (
+            <Flex
+                justify="center"
+                align="center"
+                style={{ height: "100vh", background: "linear-gradient(to right, #0D6E6E, #683598)" }}
             >
-                <Flex direction="row" wrap="wrap" gap={{ base: "40px", sm: "40px" }}>
-                    <Flex
-                        direction="column"
-                        align="flex-start"
-                        p={"20px"}
-                    >
-                        <ScaffledLogo size={64} variant="full" color="original" />
-
-                        <Image
-                            src={mail.src}
-                            alt="mail"
-                            style={{ width: "100%", maxWidth: 300 }}
-                        />
-                    </Flex>
-
-                    <Flex
-                        direction="column"
-                        justify="space-between" // Distribute space vertically
-                        align="stretch" // Stretch horizontally
-                        p="20x"
-                    >
+                <Paper
+                    shadow="md"
+                    radius="md"
+                    p={{ base: "md", sm: "xl" }}
+                    style={{
+                        width: "60%",
+                        background: "white",
+                    }}
+                >
+                    <Flex direction="row" wrap="wrap" gap={{ base: "40px", sm: "40px" }}>
                         <Flex
                             direction="column"
+                            align="flex-start"
+                            p={"20px"}
                         >
-                            <Text size="xl" mb="sm" style={{ fontWeight: 700 }}>
-                                Email Confirmation
-                            </Text>
-                            <Text size="md" mb="md">
-                                We sent a confirmation email to: <strong>{email}</strong>
-                                <br />
-                                Check your email and enter the code to continue.
-                            </Text>
-                            <PinInput length={6} inputMode="numeric" size="lg" mb="lg" onChange={(value) => setOtp(value)} />
+                            <ScaffledLogo size={64} variant="full" color="original" />
+    
+                            <Image
+                                src={mail.src}
+                                alt="mail"
+                                style={{ width: "100%", maxWidth: 300 }}
+                            />
                         </Flex>
-
-                        <Flex justify="space-between">
-                            <Button color="gray" size="md" radius="sm" onClick={() => {
-                                notifications.show({
-                                    title: "Resending OTP",
-                                    message: "Please check your email.",
-                                    color: "blue",
-                                });
-                                setCounter(counter + 1);
-                                setOtp("");
-                            }}>
-                                Resend Code
-                            </Button>
-                            <Button color="purple" size="md" radius="sm" onClick={() => verifyEmailOTP(email, otp)}>
-                                Continue
-                            </Button>
+    
+                        <Flex
+                            direction="column"
+                            justify="space-between" // Distribute space vertically
+                            align="stretch" // Stretch horizontally
+                            p="20x"
+                        >
+                            <Flex
+                                direction="column"
+                            >
+                                <Text size="xl" mb="sm" style={{ fontWeight: 700 }}>
+                                    Email Confirmation
+                                </Text>
+                                <Text size="md" mb="md">
+                                    We sent a confirmation email to: <strong>{email}</strong>
+                                    <br />
+                                    Check your email and enter the code to continue.
+                                </Text>
+                                <PinInput length={6} inputMode="numeric" size="lg" mb="lg" onChange={(value) => setOtp(value)} />
+                            </Flex>
+    
+                            <Flex justify="space-between">
+                                <Button color="gray" size="md" radius="sm" onClick={() => {
+                                    notifications.show({
+                                        title: "Resending OTP",
+                                        message: "Please check your email.",
+                                        color: "blue",
+                                    });
+                                    setCounter(counter + 1);
+                                    setOtp("");
+                                }}>
+                                    Resend Code
+                                </Button>
+                                <Button color="purple" size="md" radius="sm" onClick={() => verifyEmailOTP(email, otp)}>
+                                    Continue
+                                </Button>
+                            </Flex>
                         </Flex>
                     </Flex>
-                </Flex>
-            </Paper>
-        </Flex>
-    );
+                </Paper>
+            </Flex>
+        );
+    }
 }
